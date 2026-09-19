e3b764895e5e8707c393363add05282a3313259d
Devasya-19 <devasyaraj19@gmail.com>
2026-09-19 12:52:21 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        vector<int>ans;
+        int carry=1;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]==9 && carry==1){
+                ans.push_back(0);
+                carry=1;
+            }
+            else{
+                ans.push_back(digits[i]+carry);
+                carry=0;  
+            }
+        }
+        if(carry==1){
+            ans.push_back(1);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
